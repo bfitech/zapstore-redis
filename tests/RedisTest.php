b1f622a02a50dd3e35d3acac4fb6285bb73b14d5
Septiyan Nariyanto <septiyan.nariyanto@gmail.com>
2017-05-28 19:41:11 +0700

    Create true metapackage.

--- tests/RedisTest.php
+++ tests/RedisTest.php
@@ -1,24 +1,5 @@
 <?php
 
 
-use BFITech\ZapCore\Logger;
-use BFITech\ZapStore as zs;
+require_once(__DIR__ . '/../vendor/bfitech/zapstore/tests/RedisTest.php');
 
-
-class RedisTest extends RedisConnTest {
-
-	public static $engine = 'redis';
-
-	public function test_redis() {
-		$logger = new Logger(
-			Logger::ERROR, getcwd() . '/zapstore-redis-test.log');
-		$config = json_decode(
-			file_get_contents(
-				getcwd() . '/zapstore-redis-test.config.json'),
-			true);
-		$sql = new zs\Redis($config['redis'], $logger);
-		$this->assertEquals(
-			$sql->get_connection_params()['redistype'], 'redis');
-	}
-
-}
